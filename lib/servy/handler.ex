b1c4b47ad02a86d6a8b6df914282229131926495
arthurariza <tucaariza@gmail.com>
2022-08-04 16:17:29 -0300

    6 Function Clauses

---
+++ lib/servy/handler.ex
@@ -0,0 +1,69 @@
+defmodule Servy.Handler do
+  def handle(request) do
+    request
+    |> parse
+    |> log
+    |> route
+    |> format_response
+  end
+
+  def log(conv) do
+    IO.inspect(conv)
+  end
+
+  def parse(request) do
+    [method, path, _] =
+      request
+      |> String.split("\n")
+      |> List.first()
+      |> String.split(" ")
+
+    %{method: method, path: path, resp_body: ""}
+  end
+
+  def route(conv) do
+    route(conv, conv.method, conv.path)
+  end
+
+  def route(conv, "GET", "/wildthings") do
+    %{conv | resp_body: "Bears, Lions, Tigers"}
+  end
+
+  def route(conv, "GET", "/bears") do
+    %{conv | resp_body: "Polar Bear, Brown Bear, Black Bear"}
+  end
+
+  def format_response(conv) do
+    """
+    HTTP/1.1 200 OK
+    Content-Type: text/html
+    Content-Length: #{byte_size(conv.resp_body)}
+
+    #{conv.resp_body}
+    """
+  end
+end
+
+request = """
+GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
+
+request = """
+GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
