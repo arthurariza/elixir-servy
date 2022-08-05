8370fe2cb52a208ed325cb5a0112178e4603fc02
arthurariza <tucaariza@gmail.com>
2022-08-05 18:17:22 -0300

    Read an HTML Form File

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -7,7 +7,7 @@ defmodule Servy.Handler do
     |> rewrite_path
     |> log
     |> route
-    |> decorate
+    # |> decorate
     |> track
     |> format_response
   end
@@ -55,6 +55,13 @@ defmodule Servy.Handler do
     %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
   end
 
+  def route(%{method: "GET", path: "/bears/new"} = conv) do
+    Path.expand("../../pages", __DIR__)
+    |> Path.join("form.html")
+    |> File.read()
+    |> handle_file(conv)
+  end
+
   def route(%{method: "GET", path: "/bears/" <> id} = conv) do
     %{conv | status: 200, resp_body: "Bear #{id}"}
   end
@@ -64,18 +71,6 @@ defmodule Servy.Handler do
     |> Path.join("about.html")
     |> File.read()
     |> handle_file(conv)
-  end
-
-  def handle_file({:ok, content}, conv) do
-    %{conv | status: 200, resp_body: content}
-  end
-
-  def handle_file({:error, :enoent}, conv) do
-    %{conv | status: 404, resp_body: "File not found"}
-  end
-
-  def handle_file({:error, reason}, conv) do
-    %{conv | status: 500, resp_body: "File Error #{reason}"}
   end
 
   # def route(%{method: "GET", path: "/about"} = conv) do
@@ -109,6 +104,18 @@ defmodule Servy.Handler do
 
     #{conv.resp_body}
     """
+  end
+
+  def handle_file({:ok, content}, conv) do
+    %{conv | status: 200, resp_body: content}
+  end
+
+  def handle_file({:error, :enoent}, conv) do
+    %{conv | status: 404, resp_body: "File not found"}
+  end
+
+  def handle_file({:error, reason}, conv) do
+    %{conv | status: 500, resp_body: "File Error #{reason}"}
   end
 
   defp status_reason(code) do
@@ -209,6 +216,18 @@ IO.puts(response)
 
 request = """
 GET /about HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
+
+request = """
+GET /bears/new HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
